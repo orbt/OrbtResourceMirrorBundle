a73aa83bd753111547ea43a73b97bf4151386baa
Taihao Zhang <jason@zth.me>
2012-10-30 11:57:55 +1100

    Add compiler pass to register resource subscribers.

---
+++ DependencyInjection/Compiler/RegisterResourceSubscribersPass.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Orbt\Bundle\ResourceMirrorBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class RegisterResourceSubscribersPass implements CompilerPassInterface
+{
+    /**
+     * Registers resource event subscribers.
+     */
+    public function process(ContainerBuilder $container)
+    {
+        if (!$container->hasDefinition('event_dispatcher')) {
+            return;
+        }
+
+        $definition = $container->getDefinition('event_dispatcher');
+
+        foreach ($container->findTaggedServiceIds('orbt_resource_mirror.resource_subscriber') as $id => $attributes) {
+            $class = $container->getDefinition($id)->getClass();
+
+            $refClass = new \ReflectionClass($class);
+            $interface = 'Symfony\Component\EventDispatcher\EventSubscriberInterface';
+            if (!$refClass->implementsInterface($interface)) {
+                throw new \InvalidArgumentException(sprintf('Service "%s" must implement interface "%s".', $id, $interface));
+            }
+
+            $definition->addMethodCall('addSubscriberService', array($id, $class));
+        }
+    }
+}
--- OrbtResourceMirrorBundle.php
+++ OrbtResourceMirrorBundle.php
@@ -3,7 +3,14 @@
 namespace Orbt\Bundle\ResourceMirrorBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class OrbtResourceMirrorBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new DependencyInjection\Compiler\RegisterResourceSubscribersPass());
+    }
 }
